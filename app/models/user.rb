31d78ae51f2eb150e7221264360b17769ea218bd
RailsProject <kambojgurpreet32@gmail.com>
2020-05-08 23:09:32 +0530

    user model created and fields validated

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :username, presence: true, length: {minimum:3, maximum:15},
+            uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email , presence: true, length:{maximum:100},
+            uniqueness: {case_sensitive: false},
+            format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
+end
