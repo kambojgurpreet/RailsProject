445fa57e91444bc3e63a56ca9498cda593b13bd8
RailsProject <kambojgurpreet32@gmail.com>
2020-05-09 15:04:14 +0530

    login page functionality completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,22 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Something is wrong with the info, please try again!!"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to login_path
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :username, presence: true, length: {minimum:3, maximum:15},
             uniqueness: { case_sensitive: false }
+  has_many :messages
+  has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email , presence: true, length:{maximum:100},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}
-  has_many :messages
-  has_secure_password
 end
