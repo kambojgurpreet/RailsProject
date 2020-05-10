7c51eeb9e6d5808dfa4307c861418d6429e914fc
RailsProject <kambojgurpreet32@gmail.com>
2020-05-10 21:51:45 +0530

    login after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
     #render plain: params[:user].inspect
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "New account created successfully!"
-      redirect_to login_path
+      redirect_to root_path
     else
       render :new
     end
